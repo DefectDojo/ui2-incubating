40cd6770047e95f8fefe17d58262637b6de264ab
Aniket Bhat <64396289+AniketBhat@users.noreply.github.com>
2021-06-28 13:33:26 +0530

    Scaffolding added SideBar and Initial Navigation (#5)
    * Update README.md documentation
    
    * added scaffolding for routing and side navigation bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import Main from "./components/Main/main";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route path="/dashboard" component={Main} />
+          <Route path="/product" component={Main} />
+          <Route path="/engagements" component={Main} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
