5d47a1967ff21c50decbd409dc7320fd2874f78f
Aniket Bhat <aniketbhat1072@gmail.com>
2021-07-11 13:02:11 +0530

    UIV2-33 Added Private Router to protect data routes

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
-import React, { useState } from 'react';
-import { IntlProvider } from 'react-intl';
-import Layout from './views/Layout';
-import messages from './messages';
-import { BrowserRouter as Router, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route} from 'react-router-dom';
 import './styles/App.scss';
-import {RouteWithSubRoutes} from './routes'
-import {routes} from './routes'
+import { PrivateRoute } from './_components/PrivateRoute';
+import Product from './views/Product';
+import Login from './LoginPage/Login';
+
 
 function App() {
-  const [locale, setLocale] = useState('en');
 
   return (
     <Router>
-    <IntlProvider locale={locale} messages={messages[locale]}>
-      <Layout setLocale={setLocale}/>
-      <div id="modal_root"></div>
-    </IntlProvider>
-
-    
-    <Switch>
-          {routes.map((route, i) => (
-            <RouteWithSubRoutes key={i} {...route} />
-          ))}
-    </Switch>
+      <PrivateRoute exact path="/products" component={Product}/>
+      
+      <Route path="/login" component={Login}/>
     </Router>
 
   );
