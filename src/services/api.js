b139f9ce1c01b15e89e45fd05c953106cdc53d52
Aniket Bhat <64396289+AniketBhat@users.noreply.github.com>
2021-07-25 10:51:37 -0700

    Convert from Mock data to Fetch Data from Backend (#14)
    * Convert from Mock data to Fetch Data from Backend
    
    * Added product type support for the table

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,38 @@
 export const baseURL = "http://localhost:8080/api/v2/"
+export const productListUrl = baseURL+"products/"
+export const productTypeUrl = baseURL+"product_types/"
+
+export function FetchProductsData(setRowData){
+    var currentToken = localStorage.getItem("token")
+    return fetch(productListUrl ,{
+      method: 'get',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': "Token "+currentToken,
+      })
+    })
+    .then(res => res.json())
+    .then(res => res["results"])
+    .then(async(res) => {
+        return await Promise.all(res.map(async(response) => {
+            const typepromise = await FetchProductTypeNameById(response["prod_type"])
+            const type = await typepromise.json()
+            response["prod_type"] = type["name"]
+            return response
+        }))
+    })
+    .then(res => setRowData(res))
+    
+
+}
+
+function FetchProductTypeNameById(id){
+    var currentToken = localStorage.getItem("token")
+    return fetch(productTypeUrl+id+"/", {
+        method: 'get',
+        headers: new Headers({
+          'Accept': 'application/json',
+          'Authorization': "Token "+currentToken,
+        })
+      });
+}
--- src/views/Product.js
+++ src/views/Product.js
@@ -1,57 +1,158 @@
 import React from "react";
 import { DataGrid, GridToolbar } from '@material-ui/data-grid';
-import { makeStyles } from '@material-ui/styles';
-import { column, row } from "./jsonFile";
+import { withStyles } from "@material-ui/styles";
 import { Fragment } from "react";
+import {FetchProductsData} from "../services/api.js";
+import StarIcon from '@material-ui/icons/Star';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+import Tooltip from '@material-ui/core/Tooltip';
+import Chip from '@material-ui/core/Chip';
 
-const columns = column
-const rows = row
+function criticality(str){
+  const arr={"very high": 5, "high":4, "medium":3, "low":2, "very low":1, "none":0};
+  var stack=[];
+  for (let i=0; i<arr[str]; i++){
+    stack.push(<StarIcon fontSize="small" />);
+  }
+  for (let i=arr[str]+1; i<=5; i++){
+      stack.push(<StarBorderIcon fontSize="small" />);
+  }
+  return <div>{stack}</div>;
+}
+
+
+const columns =[
+  { field: 'name', 
+  headerName: 'Product',
+  flex: 1,
+  headerAlign: 'center',
+},
+  {
+    field: 'tags',
+    headerName: 'Tags',
+    flex: 1,
+    headerAlign: 'center',
+    editable: true,
+    align:'center',
+    renderCell: (params) => {
+      // eslint-disable-next-line
+        if(params.value==''){
+          return;
+        }
+        return params.value.map(function(value, index){
+          return <Chip label={value} color="primary"></Chip>
+        })
+    }
+  },
+  {
+    field: 'business_criticality',
+    headerName: 'Criticality',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+    renderCell: (ele) => (
+        <Tooltip title={ele.value} arrow>
+            <span>{criticality(ele.value)}</span>
+        </Tooltip> 
+        ), 
+  },
+  {
+    field: 'product_meta',
+    headerName: 'Metadata',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+  {
+    field: 'engagements',
+    headerName: 'Engagements',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+  {
+    field: 'findings_count',
+    headerName: 'Active (verified) Findings',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+  {
+    field: 'endpoints',
+    headerName: 'Endpoints',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+  {
+    field: 'contact',
+    headerName: 'Contact',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+  {
+    field: 'prod_type',
+    headerName: 'Product Type',
+    flex: 1,
+    editable: true,
+    headerAlign: 'center',
+    align:'center',
+  },
+];
 
 //styling 
 // const defaultTheme = createMuiTheme();
-const useStyles = makeStyles((theme) => 
-({
+const useStyles = theme => ({
   root: {
     '& .MuiDataGrid-row.Mui-even:not(:hover)': {
-      backgroundColor:'lightgray'
+      backgroundColor:'lightgray',
     }
+  },
+});
+
+class Product extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      rows: []
+    }
+    this.setRowData = this.setRowData.bind(this);
+  }
+
+  setRowData(rowData){
+    this.setState({
+      rows: rowData
+    })
+  }
+
+  componentDidMount(){
+    FetchProductsData(this.setRowData);
+  }
+
+  render(){
+    const { classes } = this.props
+    return (
+      <Fragment>
+      <div style={{ height: 600, width: '100%', textAlign:'center' }} className={classes.root} >
+      <DataGrid
+        rows={this.state.rows}
+        columns={columns}
+        components={{
+          Toolbar: GridToolbar,
+        }}
+        pageSize={10}
+      />
+    </div>
+    </Fragment>
+    );
   }
 }
-));
 
-function fetchData(){
-  const url = "https://demo.defectdojo.org/api/v2/products/"
-  fetch(url,{
-    method: 'get',
-    headers: new Headers({
-      'Accept': 'application/json',
-      'Authorization': "Token 548afd6fab3bea9794a41b31da0e9404f733e222",
-    })
-  })
-  .then(res => res.json())
-  .then(res => console.log(res))
-}
-
-function Product() {
-
-  const classes = useStyles();
-  fetchData();
-  return (
-    <Fragment>
-    <div style={{ height: 600, width: '100%', textAlign:'center' }} className={classes.root}>
-    <DataGrid
-      rows={rows}
-      columns={columns}
-      components={{
-        Toolbar: GridToolbar,
-      }}
-      pageSize={10}
-      checkboxSelection
-      disableSelectionOnClick
-    />
-  </div>
-  </Fragment>
-  );
-}
-
-export default Product;
+export default withStyles(useStyles)(Product);
