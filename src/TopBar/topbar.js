df29a6e100f2371526cac2a99ae24385256ecb9a
Aniket Bhat <aniketbhat1072@gmail.com>
2021-07-25 23:52:57 +0530

    Cleanup dependencies

--- src/TopBar/topbar.js
+++ src/TopBar/topbar.js
@@ -1,5 +1,5 @@
 import React,{ Component } from "react";
-import { Navbar, Container, Nav, Dropdown} from "react-bootstrap";
+import { Navbar, Nav, Dropdown} from "react-bootstrap";
 import { withRouter } from "react-router";
 import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import defectdojologo from '../images/defectdojo.png'
