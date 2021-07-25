f5261589e8f5af87f8fc657ec28bb0b37f19331e
Aniket Bhat <aniketbhat1072@gmail.com>
2021-07-25 22:29:31 +0530

    Convert from Mock data to Fetch Data from Backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,1 +1,15 @@
-export const baseURL = "http://localhost:8080/api/v2/"
+export const baseURL = "http://localhost:8080/api/v2"
+export const productListUrl = baseURL+"/products/"
+
+export function FetchProductsData(setRowData){
+    var currentToken = localStorage.getItem("token")
+    return fetch(productListUrl ,{
+      method: 'get',
+      headers: new Headers({
+        'Accept': 'application/json',
+        'Authorization': "Token "+currentToken,
+      })
+    })
+    .then(res => res.json())
+    .then(res => setRowData(res["results"]))
+  }
