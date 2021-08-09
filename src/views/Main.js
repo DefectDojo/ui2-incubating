472de2f50924e59d33fcbd95742bcfbd99d65f9c
Aniket Bhat <aniketbhat1072@gmail.com>
2021-08-10 00:34:21 +0530

    restructure modal components

--- src/views/Main.js
+++ src/views/Main.js
@@ -1,11 +1,11 @@
 import React from 'react'
-import ModalTest from './Modal.js';
 import { Switch } from "react-router"
 import Product from "./Product"
 import Engagement from "./Engagement"
 import { PrivateRoute } from '../_components/PrivateRoute';
 import TopBar  from "../TopBar/topbar.js";
 import {withRouter} from "react-router-dom";
+import AddProduct from './AddProduct';
 
 class Main extends React.Component{
     constructor(props){
@@ -31,12 +31,12 @@ class Main extends React.Component{
         <TopBar/>
         <Switch location={isModal ? this.previousLocation : location} >
         <PrivateRoute exact path='/products' component={Product} />
-        <PrivateRoute exact path='/product/add' component={ModalTest}/> 
+        <PrivateRoute exact path='/product/add' component={AddProduct}/> 
         <PrivateRoute key="engagement_all" exact path='/engagements/all' component={Engagement}/> 
         <PrivateRoute key="engagement_active" exact path='/engagements/active' component={Engagement}/> 
         </Switch>
         {isModal
-        ? <PrivateRoute exact path="/product/add" component={ModalTest} />
+        ? <PrivateRoute exact path="/product/add" component={AddProduct} />
         : null
          }
         </div>
