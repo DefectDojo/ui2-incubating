65246598d20b22e27e341902a426b58f1929974b
Aniket Bhat <aniketbhat1072@gmail.com>
2021-08-10 00:18:54 +0530

    Include extra fields for Add Product Type Modal

--- src/views/Modal.js
+++ src/views/Modal.js
@@ -1,14 +1,62 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Modal, Button, Form } from 'react-bootstrap';
+import { CreateNewProduct, FetchProductTypes, FetchUsers } from '../services/api';
+import { TrendingDown } from '@material-ui/icons';
 
 class ModalTest extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.productTypeRef = React.createRef()
+    this.technicalContactRef = React.createRef()
+    this.productManagerRef = React.createRef()
+    this.teamManagerRef = React.createRef()
+    this.businessCriticalityRef = React.createRef()
+    this.onFormSubmit = this.onFormSubmit.bind(this)
+  }
+  
+  createProductTypeOptions(){
+    const selectElement =document.getElementById("product_type")
+    FetchProductTypes()
+    .then(res => res.json())
+    .then(res=> res["results"])
+    .then(res => res.map(arr => {
+      selectElement.append(new Option(arr["name"], arr["id"]))
+    }));
+  }
+
+  createUsersTypeOptions(){
+    const one = document.getElementById("technical_contact");
+    const two = document.getElementById("team_manager");
+    const three = document.getElementById("product_manager");
+
+    FetchUsers()
+    .then(res => res.json())
+    .then(res => res["results"])
+    .then(res => res.map(arr => {
+      one.append(new Option(arr["username"], arr["id"]));
+      two.append(new Option(arr["username"], arr["id"]));
+      three.append(new Option(arr["username"], arr["id"]));
+    }));
+  }
+
+  componentDidMount(){
+    this.createProductTypeOptions()
+    this.createUsersTypeOptions()
+  }
 
   onFormSubmit(e){
     e.preventDefault()
     const formData = new FormData(e.target)
-     var formDataObj = Object.fromEntries(formData.entries())
-    console.log(formDataObj)
+    var formDataObj = Object.fromEntries(formData.entries())
+    formDataObj["prod_type"]=this.productTypeRef.current.value;
+    formDataObj["team_manager"]=this.teamManagerRef.current.value;
+    formDataObj["product_manager"]=this.productManagerRef.current.value;
+    formDataObj["technical_contact"]=this.technicalContactRef.current.value;
+    formDataObj["business_criticality"]=this.businessCriticalityRef.current.value;
+    CreateNewProduct(formDataObj);
+    this.props.history.goBack();
   }
 
   render(){
@@ -18,31 +66,75 @@ class ModalTest extends React.Component {
       className="modal-wrapper"
       onClick={() => this.props.history.goBack()}
     >
-      <Modal.Dialog onClick={e => e.stopPropagation()}>
+      <Modal.Dialog size="lg" onClick={e => e.stopPropagation()}>
       <Modal.Header>
         <Modal.Title>Add Product</Modal.Title>
       </Modal.Header>
     
       <Modal.Body>
-      <Form onSubmit={this.onFormSubmit}>
+        <Form onSubmit={this.onFormSubmit}>
             <Form.Group className="mb-3">
               <Form.Label>Name</Form.Label>
               <Form.Control type="text" name="name" placeholder="Enter Product Name" />
             </Form.Group>
           
-            <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Group className="mb-3">
               <Form.Label>Description</Form.Label>
-              <Form.Control type="text" name="description" placeholder="Password" />
+              <Form.Control type="text" name="description" placeholder="Description" />
             </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label> Product Manager</Form.Label>
+              <Form.Control id="product_manager" as="select" ref={this.productManagerRef}> 
+                <option value="0"> ------------- </option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label> Technical Contact</Form.Label>
+              <Form.Control id="technical_contact" as="select" ref={this.technicalContactRef}> 
+                <option value="0"> ------------- </option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label> Team Manager</Form.Label>
+              <Form.Control id="team_manager" as="select" ref={this.teamManagerRef}> 
+                <option value="0"> ------------- </option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label> Product Type</Form.Label>
+              <Form.Control id="product_type" as="select" ref={this.productTypeRef}> 
+                <option value="0"> ------------- </option>
+              </Form.Control>
+            </Form.Group>
+
+            <Form.Group className="mb-3">
+              <Form.Label> Business Criticality </Form.Label>
+              <Form.Control as="select" ref={this.businessCriticalityRef}> 
+                <option value="none"> ------------- </option>
+                <option value="very high">Very High</option>
+                <option value="high">High</option>
+                <option value="medium">Medium</option>
+                <option value="low">Low</option>
+                <option value="very low">Very Low</option>
+              </Form.Control>
+            </Form.Group>
+
+
+
+            <br/>
 
             <Button variant="primary" type="submit">
               Submit
             </Button>
-</Form> 
+            <Button variant="secondary" onClick={this.props.history.goBack}>
+              Cancel
+            </Button>
+        </Form> 
       </Modal.Body>
-    
-      <Modal.Footer>
-      </Modal.Footer>
     </Modal.Dialog>
   </div>
 );
